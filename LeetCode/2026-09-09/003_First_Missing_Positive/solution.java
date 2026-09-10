9f8bab4b96b40a013acb10f7487f3edd2406876d
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-10 09:35:04 +0530

    Implement firstMissingPositive method logic

--- LeetCode/2026-09-09/003_First_Missing_Positive/solution.java
+++ LeetCode/2026-09-09/003_First_Missing_Positive/solution.java
@@ -1,3 +1,10 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
         int n = nums.length;
+        for (int i = 0; i < n; i++) {
+
+            while (nums[i] >= 1 &&
+                   nums[i] <= n &&
+                   nums[nums[i] - 1] != nums[i]) {
+
+                int correctIndex = nums[i] - 1;
