3a5b1dbc10090c05939eb5168e577f2ba4125ff8
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-10 18:32:49 +0530

    Count numbers with an even number of digits

--- LeetCode/2026-09-09/006_Find_Numbers_with_Even_Number_of_Digits/solution.java
+++ LeetCode/2026-09-09/006_Find_Numbers_with_Even_Number_of_Digits/solution.java
@@ -4,3 +4,7 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
 
             int digits = String.valueOf(nums[i]).length();
+            if (digits % 2 == 0) {
+                count++;
+            }
+        }
