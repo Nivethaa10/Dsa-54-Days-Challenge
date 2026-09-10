eeda98907fed5a3f00e609387cbfe897e5786298
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-10 18:44:31 +0530

    Correct index variable name in shuffle logic
    Fix typo in index variable name to ensure correct assignment.

--- LeetCode/2026-09-09/005_Shuffle_the_Array/solution.java
+++ LeetCode/2026-09-09/005_Shuffle_the_Array/solution.java
@@ -5,7 +5,7 @@ class Solution {
 
         for (int i = 0; i < n; i++) {
 
-            result[index] = nums[i];
+            result[indexs] = nums[i];
             index++;
             result[index] = nums[i + n];
             index++;
