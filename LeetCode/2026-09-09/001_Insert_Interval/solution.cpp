15443e6f3f77bcd4de4e9990b5c5148fff4f3c1a
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-10 09:24:28 +0530

    Add newInterval to the list of intervals

--- LeetCode/2026-09-09/001_Insert_Interval/solution.cpp
+++ LeetCode/2026-09-09/001_Insert_Interval/solution.cpp
@@ -1,2 +1,5 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> intervalList = new ArrayList<>(Arrays.asList(intervals)); 
+        intervalList.add(newInterval); 
+
