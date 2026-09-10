ea8d8e1f626d0032153f63d9dff0596c2bbff57b
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-10 09:29:23 +0530

    Add initial implementation of string compression

--- LeetCode/2026-09-09/002_String_Compression/solution.java
+++ LeetCode/2026-09-09/002_String_Compression/solution.java
@@ -1,3 +1,8 @@
 class Solution {
     public int compress(char[] chars) {
         int ans = 0;
+        for (int i = 0; i < chars.length;) {
+            final char letter = chars[i];
+            int count = 0;
+
+        
