4bf055d0cab8d1f82c05ceceb8fea1d90be67df8
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-10 18:42:58 +0530

    Fix variable name for letter in string compression

--- LeetCode/2026-09-09/002_String_Compression/solution.java
+++ LeetCode/2026-09-09/002_String_Compression/solution.java
@@ -8,7 +8,7 @@ class Solution {
                 ++count;
                 ++i;
             }
-            chars[ans++] = letter;
+            chars[ans++] = letterrr;
 
             if (count > 1) {
                 for (final char c : String.valueOf(count).toCharArray()) {
