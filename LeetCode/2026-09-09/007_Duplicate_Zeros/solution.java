334f5a0876896ce062c8d3aef38b6fdb84baaaaf
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-10 18:35:01 +0530

    Add zero counting logic in duplicateZeros method
    Count the number of zeros in the array.

--- LeetCode/2026-09-09/007_Duplicate_Zeros/solution.java
+++ LeetCode/2026-09-09/007_Duplicate_Zeros/solution.java
@@ -1,3 +1,10 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
         int n = arr.length;
+        int zeros = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 0) {
+                zeros++;
+            }
+        }
