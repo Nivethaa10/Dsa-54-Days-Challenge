e0158433ea963f479dd3800807f70ea7a9f45985
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-10 09:24:48 +0530

    Sort intervals after adding newInterval
    Add sorting for intervals after inserting newInterval

--- LeetCode/2026-09-09/001_Insert_Interval/solution.cpp
+++ LeetCode/2026-09-09/001_Insert_Interval/solution.cpp
@@ -2,4 +2,8 @@ class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         List<int[]> intervalList = new ArrayList<>(Arrays.asList(intervals)); 
         intervalList.add(newInterval); 
+        Collections.sort(intervalList, (a, b) -> Integer.compare(a[0], b[0])); 
+ 
+        List<int[]> res = new ArrayList<>(); 
+        int[] current = intervalList.get(0); 
 
