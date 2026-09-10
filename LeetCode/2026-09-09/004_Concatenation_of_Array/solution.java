510427aa21f31ab53b0a134eef0dab449a940271
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-10 18:43:45 +0530

    Fix index error in array concatenation

--- LeetCode/2026-09-09/004_Concatenation_of_Array/solution.java
+++ LeetCode/2026-09-09/004_Concatenation_of_Array/solution.java
@@ -6,7 +6,7 @@ class Solution {
 
         for (int i = 0; i < n; i++) {
             ans[i] = nums[i];
-            ans[i + n] = nums[j];
+            ans[i + n] = nums[i];
         }
         return ans;
         
