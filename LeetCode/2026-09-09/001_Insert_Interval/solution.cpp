d5febeb41693ece29e16bb455978fefa36bdae4a
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-10 09:25:18 +0530

    Implement interval merging in insert function

--- LeetCode/2026-09-09/001_Insert_Interval/solution.cpp
+++ LeetCode/2026-09-09/001_Insert_Interval/solution.cpp
@@ -6,4 +6,14 @@ class Solution {
  
         List<int[]> res = new ArrayList<>(); 
         int[] current = intervalList.get(0); 
+        for (int i = 1; i < intervalList.size(); i++) { 
+            int[] interval = intervalList.get(i); 
+             
+            if (current[1] >= interval[0]) { 
+                current[1] = Math.max(current[1], interval[1]); 
+            } else { 
+                res.add(current); 
+                current = interval; 
+            } 
+        }
 
