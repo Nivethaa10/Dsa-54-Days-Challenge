709df401d91e0dcf0912ff7ca75f19a56e011c86
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-13 19:10:57 +0530

    Update solution.java

--- LeetCode/2026-09-10/006_Find_Minimum_in_Rotated_Sorted_Array/solution.java
+++ LeetCode/2026-09-10/006_Find_Minimum_in_Rotated_Sorted_Array/solution.java
@@ -5,3 +5,5 @@ class Solution {
         int left = 0, right = n;
         while (left < right) {
             int mid = (left + right) >> 1;      
+            if (nums[mid] > last) left = mid + 1;
+            else right = mid;
