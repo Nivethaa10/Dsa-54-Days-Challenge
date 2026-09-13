818c3409e9cc0d0f2b8add8ee3e75f20e0d091b0
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-13 19:42:03 +0530

    Initialize prefix sum and count in subarraysDivByK

--- LeetCode/2026-09-11/004_Subarray_Sums_Divisible_By_K/solution.java
+++ LeetCode/2026-09-11/004_Subarray_Sums_Divisible_By_K/solution.java
@@ -2,3 +2,7 @@ class Solution {
     public int subarraysDivByK(int[] nums, int k) {
         HashMap<Integer,Integer> map=new HashMap<>();
         map.put(0,1);
+        int prefix_sum=0;
+        int count=0;
+        for(int i=0; i<nums.length; i++){
+            prefix_sum+=nums[i];
