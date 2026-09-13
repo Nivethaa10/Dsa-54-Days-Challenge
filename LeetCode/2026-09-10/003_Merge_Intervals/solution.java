7523441109d988ba6e64729fedcd93165e779682
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-13 18:56:14 +0530

    Implement merge intervals function

--- LeetCode/2026-09-10/003_Merge_Intervals/solution.java
+++ LeetCode/2026-09-10/003_Merge_Intervals/solution.java
@@ -3,3 +3,5 @@ class Solution {
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
         List<int[]> merged = new ArrayList<>();
         int[] prev = intervals[0];
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] <= prev[1]) {
