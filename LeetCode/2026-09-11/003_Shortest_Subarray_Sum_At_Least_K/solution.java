c75b947f24b0767821a7cbda7c663ac95410e84c
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-13 19:44:59 +0530

    Add prefix sum array for shortest subarray calculation

--- LeetCode/2026-09-11/003_Shortest_Subarray_Sum_At_Least_K/solution.java
+++ LeetCode/2026-09-11/003_Shortest_Subarray_Sum_At_Least_K/solution.java
@@ -2,3 +2,6 @@ class Solution {
     public int shortestSubarray(int[] nums, int k) {
         int nLen = nums.length;
         int shortest = nLen + 1;
+        long[] prefixSum = new long[nLen + 1];
+        for(int i=0; i < nLen; i++)
+            prefixSum[i+1] = prefixSum[i] + nums[i];
