86d144e39ec5fd75fef4f90f6fa61ba129836ac6
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-13 19:14:43 +0530

    Implement subtree calculations in dfs method
    Add left and right subtree calculations in dfs method.

--- LeetCode/2026-09-10/007_Count_Nodes_Equal_to_Average_of_Subtree/solution.java
+++ LeetCode/2026-09-10/007_Count_Nodes_Equal_to_Average_of_Subtree/solution.java
@@ -6,3 +6,5 @@ class Solution {
     }
     private int[] dfs(TreeNode node) {
         if (node == null) return new int[]{0, 0};
+        int[] left = dfs(node.left);
+        int[] right = dfs(node.right);
