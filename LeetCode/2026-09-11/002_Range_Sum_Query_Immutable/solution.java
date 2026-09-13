7603903f957f2084b7b60ca7210ebf374854fe3e
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-13 19:53:27 +0530

    Fix formatting and add comments in solution.java

--- LeetCode/2026-09-11/002_Range_Sum_Query_Immutable/solution.java
+++ LeetCode/2026-09-11/002_Range_Sum_Query_Immutable/solution.java
@@ -11,3 +11,10 @@ class NumArray {
         if (left == 0) return preSum[right];
         return preSum[right] - preSum[left-1];
     }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
