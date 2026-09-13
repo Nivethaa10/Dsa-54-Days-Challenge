a6628e3bd5cffbb595ab99a4df6e9f26fe31a2bf
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-13 18:56:28 +0530

    Merge overlapping intervals in solution.java

--- LeetCode/2026-09-10/003_Merge_Intervals/solution.java
+++ LeetCode/2026-09-10/003_Merge_Intervals/solution.java
@@ -5,3 +5,4 @@ class Solution {
         int[] prev = intervals[0];
         for (int i = 1; i < intervals.length; i++) {
             if (intervals[i][0] <= prev[1]) {
+                prev[1] = Math.max(prev[1], intervals[i][1]);
