c14816912b19b5d312690a9db730c1cca9b2cad4
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-13 18:56:03 +0530

    Initialize merged list and set previous interval

--- LeetCode/2026-09-10/003_Merge_Intervals/solution.java
+++ LeetCode/2026-09-10/003_Merge_Intervals/solution.java
@@ -1,3 +1,5 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        List<int[]> merged = new ArrayList<>();
+        int[] prev = intervals[0];
