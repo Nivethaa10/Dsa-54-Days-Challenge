7c53f59a568d6da2a3fa8dae088d42b1779c738f
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-13 19:03:50 +0530

    Update solution.java

--- LeetCode/2026-09-10/005_Koko_Eating_Bananas/solution.java
+++ LeetCode/2026-09-10/005_Koko_Eating_Bananas/solution.java
@@ -3,3 +3,5 @@ class Solution {
         int left = 1;
         int right = Arrays.stream(piles).max().getAsInt();
         int ans = right;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
