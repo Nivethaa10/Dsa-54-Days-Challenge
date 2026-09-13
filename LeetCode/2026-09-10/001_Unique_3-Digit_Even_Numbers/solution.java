e3619194dc721d37e8fa45366c8585f111d52993
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-13 18:48:59 +0530

    Add logic to count unique 3-digit even numbers

--- LeetCode/2026-09-10/001_Unique_3-Digit_Even_Numbers/solution.java
+++ LeetCode/2026-09-10/001_Unique_3-Digit_Even_Numbers/solution.java
@@ -3,3 +3,6 @@ class Solution {
         int[] f = new int[10];
         int res = 0;
         for (int d : digits) f[d]++;
+        for (int i = 1; i < 10; i++) 
+            for (int j = 0; j < 10; j++) 
+                for (int k = 0; k < 9; k += 2) {
