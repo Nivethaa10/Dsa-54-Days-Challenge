8d445c46c1509e68baa6485d347f670c60449800
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-13 19:00:17 +0530

    Day 2: Calculate weight bounds
    Calculate the minimum and maximum possible shipping capacity using the largest package and total weight.

--- LeetCode/2026-09-10/004_Capacity_To_Ship_Packages/solution.java
+++ LeetCode/2026-09-10/004_Capacity_To_Ship_Packages/solution.java
@@ -1,2 +1,7 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
+        int maxWeight = -1, totalWeight = 0;
+        for (int weight : weights) {
+            maxWeight = Math.max(maxWeight, weight);
+            totalWeight += weight;
+        }
