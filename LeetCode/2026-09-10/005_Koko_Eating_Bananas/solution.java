b2a8cec5d5e3ca32c3295eb439d2a1d489f6fe1a
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-13 19:04:33 +0530

    Implement binary search for Koko's eating speed

--- LeetCode/2026-09-10/005_Koko_Eating_Bananas/solution.java
+++ LeetCode/2026-09-10/005_Koko_Eating_Bananas/solution.java
@@ -5,3 +5,10 @@ class Solution {
         int ans = right;
         while (left <= right) {
             int mid = left + (right - left) / 2;
+            if (canFinish(piles, h, mid)) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
