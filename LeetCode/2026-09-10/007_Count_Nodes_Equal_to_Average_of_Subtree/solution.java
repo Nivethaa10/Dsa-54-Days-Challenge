2939e873ea872de2e3d849e0db183ec74d3f0d26
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-13 19:14:58 +0530

    Calculate sum and count in DFS for subtree average

--- LeetCode/2026-09-10/007_Count_Nodes_Equal_to_Average_of_Subtree/solution.java
+++ LeetCode/2026-09-10/007_Count_Nodes_Equal_to_Average_of_Subtree/solution.java
@@ -8,3 +8,5 @@ class Solution {
         if (node == null) return new int[]{0, 0};
         int[] left = dfs(node.left);
         int[] right = dfs(node.right);
+        int sum = left[0] + right[0] + node.val;
+        int cnt = left[1] + right[1] + 1;
