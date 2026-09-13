c765a7db98c9fb2885ffb7710345824c4277d234
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-13 18:33:32 +0530

    Merge two sorted arrays into one

--- LeetCode/2026-09-10/MedianOfTwoSortedArrays.java
+++ LeetCode/2026-09-10/MedianOfTwoSortedArrays.java
@@ -4,3 +4,10 @@ class Solution {
         int m = nums2.length;
         int[] merged = new int[n + m];
         int k = 0;
+        for (int i = 0; i < n; i++) {
+            merged[k++] = nums1[i];
+        }
+
+        for (int i = 0; i < m; i++) {
+            merged[k++] = nums2[i];
+        }
