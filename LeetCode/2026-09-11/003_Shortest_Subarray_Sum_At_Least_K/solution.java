a620aba7279583d27d34e1f19573a1b53d723452
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-13 19:45:26 +0530

    Fix loop condition to include last prefix sum index

--- LeetCode/2026-09-11/003_Shortest_Subarray_Sum_At_Least_K/solution.java
+++ LeetCode/2026-09-11/003_Shortest_Subarray_Sum_At_Least_K/solution.java
@@ -6,3 +6,6 @@ class Solution {
         for(int i=0; i < nLen; i++)
             prefixSum[i+1] = prefixSum[i] + nums[i];
         Deque<Integer> startIdxs = new ArrayDeque<>();
+        for(int i=0; i < nLen + 1; i++){
+            while(!startIdxs.isEmpty() && prefixSum[i] - prefixSum[startIdxs.peek()] >= k)
+                shortest = Math.min(shortest, i - startIdxs.poll());
