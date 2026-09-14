c78dd58e0365a166e6210a2626799508c0c3f59b
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-14 20:28:31 +0530

    Implement reverseStr method for string reversal

--- LeetCode/2026-09-13/004_Reverse_String_II/solution.java
+++ LeetCode/2026-09-13/004_Reverse_String_II/solution.java
@@ -2,3 +2,7 @@ class Solution {
     public String reverseStr(String s, int k) {
         int step = k * 2;
         char[] arr = s.toCharArray();
+        for(int i = 0; i < arr.length; i += step){
+
+            int start = i;
+            int end = Math.min(i + k - 1, arr.length - 1);
