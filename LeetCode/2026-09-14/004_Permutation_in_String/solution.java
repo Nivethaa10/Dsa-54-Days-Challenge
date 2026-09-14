e5a4b9e5fcc163288cf21fcdc293649d247f9c9a
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-14 20:57:36 +0530

    Initialize character frequency arrays

--- LeetCode/2026-09-14/004_Permutation_in_String/solution.java
+++ LeetCode/2026-09-14/004_Permutation_in_String/solution.java
@@ -1,2 +1,8 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
+        int n1 = s1.length();
+        int n2 = s2.length();
+        if (n1 > n2) return false;
+
+        int[] count1 = new int[26];
+        int[] count2 = new int[26];
