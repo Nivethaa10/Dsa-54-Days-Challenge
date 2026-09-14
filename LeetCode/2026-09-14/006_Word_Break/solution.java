95243bbb3312e5252a3ee66d9446c371055ea825
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-14 21:05:32 +0530

    Initialize dictionary set and dp array

--- LeetCode/2026-09-14/006_Word_Break/solution.java
+++ LeetCode/2026-09-14/006_Word_Break/solution.java
@@ -2,3 +2,6 @@ import java.util.*;
 
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> dict = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
