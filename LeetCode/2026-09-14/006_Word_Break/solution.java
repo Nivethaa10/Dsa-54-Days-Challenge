575dd9c942c299b32e0ac47d1154957f2c84ff86
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-14 21:06:00 +0530

    Add nested dynamic programming loops

--- LeetCode/2026-09-14/006_Word_Break/solution.java
+++ LeetCode/2026-09-14/006_Word_Break/solution.java
@@ -5,3 +5,6 @@ class Solution {
         Set<String> dict = new HashSet<>(wordDict);
         boolean[] dp = new boolean[s.length() + 1];
         dp[0] = true;
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
