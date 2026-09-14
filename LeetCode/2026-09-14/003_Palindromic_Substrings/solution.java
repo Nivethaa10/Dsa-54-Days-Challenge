37a7fd5b1e30c31bbe0f93aecf2a24ddccd4a321
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-14 20:54:20 +0530

    Expand around center for odd and even lengths

--- LeetCode/2026-09-14/003_Palindromic_Substrings/solution.java
+++ LeetCode/2026-09-14/003_Palindromic_Substrings/solution.java
@@ -3,3 +3,11 @@ class Solution {
 
     public int countSubstrings(String s) {
         if (s == null || s.length() == 0) return 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            extendPalindrome(s, i, i);
+            extendPalindrome(s, i, i + 1);
+        }
+
+        return count;
+    }
