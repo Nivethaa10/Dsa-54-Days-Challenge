cd2cfdb47fdee2aa01b724d53d0a7b29a71f38fa
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-14 21:01:48 +0530

    Initialize seen and repeated HashSets

--- LeetCode/2026-09-14/005_Repeated_DNA_Sequences/solution.java
+++ LeetCode/2026-09-14/005_Repeated_DNA_Sequences/solution.java
@@ -2,3 +2,5 @@ import java.util.*;
 
 class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
+        Set<String> seen = new HashSet<>();
+        Set<String> repeated = new HashSet<>();
