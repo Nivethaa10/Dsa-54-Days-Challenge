87d0c3bc1a70a6369b8046038736193699d0c03e
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-14 20:58:04 +0530

    Populate initial window and check match

--- LeetCode/2026-09-14/004_Permutation_in_String/solution.java
+++ LeetCode/2026-09-14/004_Permutation_in_String/solution.java
@@ -6,3 +6,10 @@ class Solution {
 
         int[] count1 = new int[26];
         int[] count2 = new int[26];
+
+        for (int i = 0; i < n1; i++) {
+            count1[s1.charAt(i) - 'a']++;
+            count2[s2.charAt(i) - 'a']++;
+        }
+
+        if (matches(count1, count2)) return true;
