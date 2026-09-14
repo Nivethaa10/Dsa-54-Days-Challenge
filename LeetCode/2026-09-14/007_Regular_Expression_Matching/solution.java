139268389df8d2eeb2bcbee3f5843cf73e5bfba6
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-14 21:09:16 +0530

    Handle base cases with wildcard asterisks

--- LeetCode/2026-09-14/007_Regular_Expression_Matching/solution.java
+++ LeetCode/2026-09-14/007_Regular_Expression_Matching/solution.java
@@ -4,3 +4,9 @@ class Solution {
         int n = p.length();
         boolean[][] dp = new boolean[m + 1][n + 1];
         dp[0][0] = true;
+
+        for (int j = 2; j <= n; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
