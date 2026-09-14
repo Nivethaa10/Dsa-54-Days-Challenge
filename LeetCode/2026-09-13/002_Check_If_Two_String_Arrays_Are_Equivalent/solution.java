750ca85a940e34881fca22f43d95e3b3a8c209a0
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-14 20:23:37 +0530

    Update solution.java

--- LeetCode/2026-09-13/002_Check_If_Two_String_Arrays_Are_Equivalent/solution.java
+++ LeetCode/2026-09-13/002_Check_If_Two_String_Arrays_Are_Equivalent/solution.java
@@ -1,2 +1,4 @@
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        StringBuilder s1 = new StringBuilder();
+        StringBuilder s2 = new StringBuilder();
