e42aec686fc32f2d22fea92828b7b5e38288af66
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-14 20:51:32 +0530

    Complete isPalindrome helper implementation

--- LeetCode/2026-09-14/002_Find_First_Palindromic_String_in_the_Array/solution.java
+++ LeetCode/2026-09-14/002_Find_First_Palindromic_String_in_the_Array/solution.java
@@ -15,3 +15,8 @@ class Solution {
             if (s.charAt(left) != s.charAt(right)) {
                 return false;
             }
+            left++;
+            right--;
+        }
+        return true;
+    }
