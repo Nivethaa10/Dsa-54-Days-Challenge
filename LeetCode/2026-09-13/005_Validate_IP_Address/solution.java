2564842b61d95ddb5bda1d0751fad8c1b9fb11ea
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-14 20:30:57 +0530

    Update solution.java

--- LeetCode/2026-09-13/005_Validate_IP_Address/solution.java
+++ LeetCode/2026-09-13/005_Validate_IP_Address/solution.java
@@ -1,1 +1,10 @@
-
+class Solution {
+    public String validIPAddress(String IP) {
+        if(IP.length()==0) return "Neither";
+        
+        if(IP.indexOf(".")>=0) return validateIPV4(IP);
+        
+        if(IP.indexOf(":")>=0) return validateIPV6(IP);
+        
+        return "Neither";
+    }
