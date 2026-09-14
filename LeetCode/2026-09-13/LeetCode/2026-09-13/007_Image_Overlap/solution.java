448f3e2df0b1eab06a53147cee8213acfda87a1b
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-14 20:41:26 +0530

    Add logic to collect coordinates of ones in images

--- LeetCode/2026-09-13/LeetCode/2026-09-13/007_Image_Overlap/solution.java
+++ LeetCode/2026-09-13/LeetCode/2026-09-13/007_Image_Overlap/solution.java
@@ -4,3 +4,9 @@ class Solution {
         
         List<int[]> A = new ArrayList<>();
         List<int[]> B = new ArrayList<>();
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (img1[i][j] == 1) A.add(new int[]{i, j});
+                if (img2[i][j] == 1) B.add(new int[]{i, j});
+            }
+        }
