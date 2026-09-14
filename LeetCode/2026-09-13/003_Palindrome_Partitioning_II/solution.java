1506d14cbff3de60122309be3fecd64a62ef7896
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-14 20:25:51 +0530

    Add solve method for palindrome partitioning

--- LeetCode/2026-09-13/003_Palindrome_Partitioning_II/solution.java
+++ LeetCode/2026-09-13/003_Palindrome_Partitioning_II/solution.java
@@ -2,3 +2,9 @@ class Solution {
     private int n;
     private boolean[][] palindrome;
     private int[] dp;
+    private int solve(String s, int i) {
+        if (i == n)
+            return -1;
+
+        if (dp[i] != -1)
+            return dp[i];
