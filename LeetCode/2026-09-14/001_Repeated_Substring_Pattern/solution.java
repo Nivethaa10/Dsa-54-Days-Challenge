67bbe1ea6200699d1ae234ed6c2ede9ae6e1d785
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-14 20:46:52 +0530

    Implement length check loop in solution.java

--- LeetCode/2026-09-14/001_Repeated_Substring_Pattern/solution.java
+++ LeetCode/2026-09-14/001_Repeated_Substring_Pattern/solution.java
@@ -1,3 +1,5 @@
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
         int n = s.length();
+        for (int len = n / 2; len >= 1; len--) {
+            if (n % len == 0) {
