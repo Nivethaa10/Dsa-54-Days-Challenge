b106b5c51fb9e45a82b5da474aacddd2ef7a9e40
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-14 21:06:28 +0530

    Update dp state for valid prefix segmentation

--- LeetCode/2026-09-14/006_Word_Break/solution.java
+++ LeetCode/2026-09-14/006_Word_Break/solution.java
@@ -8,3 +8,9 @@ class Solution {
 
         for (int i = 1; i <= s.length(); i++) {
             for (int j = 0; j < i; j++) {
+                if (dp[j] && dict.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
