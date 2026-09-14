a22e43697d8c3ca1e252706a37073ae234f9e07e
Nivethaa10 <nivethaaramesh10@gmail.com>
2026-09-14 20:51:04 +0530

    Add two pointer character comparison

--- LeetCode/2026-09-14/002_Find_First_Palindromic_String_in_the_Array/solution.java
+++ LeetCode/2026-09-14/002_Find_First_Palindromic_String_in_the_Array/solution.java
@@ -11,3 +11,7 @@ class Solution {
     private boolean isPalindrome(String s) {
         int left = 0;
         int right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
